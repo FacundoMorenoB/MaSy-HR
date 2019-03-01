23ee4babd35004dfba72e332d03ece341f7f4465
Facundo Moreno <facundo_yotuel@yahoo.com.mx>
2019-02-28 23:53:24 -0600

    Calendario

--- application/third_party/community_auth/views/examples/page_body_employee.php
+++ application/third_party/community_auth/views/examples/page_body_employee.php
@@ -12,24 +12,9 @@
       date_input.datepicker(options);
     })
 </script>
-
-<!--<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>-->
-
 <!-- Include Date Range Picker -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 
-<script>
-    $(document).ready(function(){
-        var date_input=$('input[name="date"]'); //our date input has the name "date"
-        var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-        date_input.datepicker({
-            format: 'mm/dd/yyyy',
-            container: container,
-            todayHighlight: true,
-            autoclose: true,
-        })
-    })
-</script>
 <div class="team-boxed">
 <div class="container"> 
  <div class="row register-form">
